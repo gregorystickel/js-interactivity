e271631c2a7ddfd43a6d58245f77b53f038c20a1
Greg Stickel <gregory.stickel@live.com>
2022-09-28 16:17:42 -0400

    Update 4:17pm

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log(`hello world`);
+const message = document.querySelector('#message');
+
+
+function addMovie (event) {
+    event.preventDefault()
+    const inputField = document.querySelector(`input`);
+    const movieList = document.querySelector(`ul`);
+    const movie = document.createElement(`li`);
+    movieList.appendChild(movie);
+    const movieTitle = document.createElement(`span`);
+    movieTitle.textContent = inputField.value;
+    addEventListener(`click`,crossOffMovie);
+    movie.appendChild(movieTitle);
+    movieList.appendChild(movie);
+    inputField.value = '';
+    
+    const deleteBTN = document.createElement('button');
+    deleteBTN.textContent = `X`;
+    deleteBTN.addEventListener(`click`, deleteMovie);
+    movie.appendChild(deleteBTN);
+
+
+}
+
+function deleteMovie (event) {
+    event.target.parentNode.remove();
+    message.textContent = `Movie deleted!`;
+
+}
+
+function crossOffMovie (event) {
+    event.target.classList.toggle(`checked`); 
+    
+
+}
+
+const movieform = document.querySelector(`form`);
+console.log(movieform)
+movieform.addEventListener(`submit`, addMovie);
+
+
+
